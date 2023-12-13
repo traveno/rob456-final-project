5de17761ee2edb48d0f627e47600c62035bb7ef5
Stephen <teamgreensf@gmail.com>
2023-12-13 07:46:53 +0000

    Code comments!

--- src/walle/src/slam.py
+++ src/walle/src/slam.py
@@ -31,18 +31,22 @@ class SLAMNode:
         # Processed map server
         self.pmap_pub = rospy.Publisher('map_processed', ProcessedMap, queue_size=1, latch=True)
 
+        # The robot will send messages that describe it's path, before its been turned
+        # into waypoints. This allows us to compare the raw path vs our optimized waypoints.
+        #   NOTE: Current path is a darker green than previous paths.
         self.path_sub = rospy.Subscriber('path', Path, self.path_update, queue_size=10)
         self.path_points = []
         self.old_path_points = []
 
     def execute(self, map):
-        rospy.loginfo(f"Received SLAM map to process")
+        rospy.loginfo(f"----BEGIN PROCESSING----\nReceived SLAM map to process")
         start = time.time()
 
         # Process raw monochrome data
         map_thresh, map_width, map_height, offset_ij = self.process_map(map)
 
         # Find good places to check out ya know, explore the world
+        # This function mutates the map, you have been warned!
         self.find_unseen(map_thresh)
 
         rospy.loginfo(f'Trimmed size: {map_width}px width, {map_height}px height')
@@ -50,6 +54,7 @@ class SLAMNode:
 
         end = time.time()
         rospy.loginfo(f"SLAM processing took {round(end - start, 3)}s")
+        
         # Send a colored image to our viewer
         self.annotate_image(map_thresh, map_width, map_height, map.info, offset_ij)
 
@@ -61,16 +66,22 @@ class SLAMNode:
         pmap.offset_j = offset_ij[1]
         pmap.meta = map.info
 
+        # We're done! Send the message
         self.pmap_pub.publish(pmap)
 
         end = time.time()
-        rospy.loginfo(f"Map msg processing took {round(end - start, 3)}s")
+        rospy.loginfo(f"Map msg processing took {round(end - start, 3)}s\n-----END PROCESSING-----")
 
     def path_update(self, path_msg: Path):
         new_path_points = np.fromiter(path_msg.path, int).reshape(-1, 2)
         self.old_path_points.extend(self.path_points)
         self.path_points = new_path_points
 
+    # This function colors and draws current path and previous path(s).
+    # Result is submitted it to the image viewer for our viewing pleasure.
+    # I think it will also help the team with debugging, and see exactly
+    # what is happening with my SLAM code in real time. Otherwise,
+    # this function has no effect on the robot's behavior.
     def annotate_image(self, map, width, height, meta, offset_ij):
         # Publish a new image to rqt_image_view
         start = time.time()
@@ -89,6 +100,8 @@ class SLAMNode:
             for p in self.path_points:
                 map_rgb[p[0], p[1]] = [64, 255, 64]
 
+        # Make lines a bit thicker since 1px wide paths are hard to see sometimes.
+        # This is done via binary dilation and is a recurring theme in my processing.
         line_mask = np.all(map_rgb == [64, 255, 64], axis=-1)
         line_dilation = ndimage.binary_dilation(line_mask, np.ones((2, 2), dtype=bool))
         line_mask = (line_mask ^ line_dilation)
@@ -99,11 +112,13 @@ class SLAMNode:
             for p in self.old_path_points:
                 map_rgb[p[0], p[1]] = [225, 225, 225]
 
+        # Make lines a bit thicker since 1px wide paths are hard to see sometimes
         line_mask = np.all(map_rgb == [225, 225, 225], axis=-1)
         line_dilation = ndimage.binary_dilation(line_mask, np.ones((2, 2), dtype=bool))
         line_mask = (line_mask ^ line_dilation)
         map_rgb[line_mask] = [225, 255, 225]
               
+        # Package up the image for our image viewer
         image = Image()
         image.data = tuple(np.flipud(map_rgb).flatten().astype(int))
         image.step = width * 3
@@ -118,23 +133,39 @@ class SLAMNode:
         rospy.loginfo(f"Image view msg processing took {round(end - start, 3)}s") 
 
     def process_map(self, map):
+        # Reshape the 1D occupancy grid for processing
         map_2d = np.fromiter(map.data, int).reshape(-1, map.info.width)
 
         # Trim out the fat
         non_negative_indices = np.where(map_2d != -1)
+
+        # Find the minimum dimensions while retaining all data
         min_row, max_row = np.min(non_negative_indices[0]), np.max(non_negative_indices[0])
         min_col, max_col = np.min(non_negative_indices[1]), np.max(non_negative_indices[1])
+
+        # Calculate map width and height
         map_width, map_height = max_col - min_col + 1, max_row - min_row + 1
+
+        # Create the trimmed map
         map_trimmed = map_2d[min_row:max_row + 1, min_col:max_col + 1]
 
+        # Now we convert map_2d's range of [-1,100] to values we're
+        # familiar with, which are [0,255]
         map_ret = np.zeros((map_height, map_width), dtype=int) + 128
         map_ret[map_trimmed == 100] = 0
         map_ret[map_trimmed == 0] = 255
 
-        # Add some padding
+        # Add some padding in case our pathfinding alg explores slightly
+        # outside of map boundaries
         map_ret = np.pad(map_ret, 5, mode='constant', constant_values=0)
 
         # Thicken the wall areas using binary dilation
+        # This appears as yellow on the map, think of it as a
+        # danger zone (or no-go zone) for pathfinding.
+        #   NOTE: This suffers from potentially cutting off narrow passages,
+        #   I think it would be fun to explore further processing that checks
+        #   for islands and draws thin bridges to ensure all areas of
+        #   the map are still accessible.
         wall_mask = map_ret == 0
         dilation_mask = ndimage.binary_dilation(wall_mask, np.ones((15, 15), dtype=bool))
         wide_walls = (dilation_mask ^ wall_mask) & ((map_ret == 255) | (map_ret == 128))
@@ -145,43 +176,18 @@ class SLAMNode:
 
     def find_unseen(self, map):
         # Binary dilation, quite handy
+        # We dilate unseen areas, XOR that with non-dilated unseen areas,
+        # then AND it with areas that are accessible. This creates the blue
+        # zones in the map.
         unseen_mask = (map == 128)
         dilation_mask = ndimage.binary_dilation(unseen_mask, np.ones((3, 3), dtype=bool))
         seen_border = (dilation_mask ^ unseen_mask) & (map == 255)
 
         # Set areas we can reach but haven't seen to 254
+        # The value is arbitrary, it just needs to be unique
         map[seen_border == 1] = 254
 
         return seen_border
-
-# Bresenham's Line Algorithm
-def bresenham_line(p1, p2):
-    x0, x1 = p1[1], p2[1]
-    y0, y1 = p1[0], p2[0]
-
-    points = []
-    dx = abs(x1 - x0)
-    dy = abs(y1 - y0)
-    sx = 1 if x0 < x1 else -1
-    sy = 1 if y0 < y1 else -1
-    err = dx - dy
-
-    while True:
-        points.append((x0, y0))
-
-        if x0 == x1 and y0 == y1:
-            break
-
-        e2 = 2 * err
-        if e2 > -dy:
-            err -= dy
-            x0 += sx
-        if e2 < dx:
-            err += dx
-            y0 += sy
-
-    return np.array(points)
-
 
 if __name__ == "__main__":
     rospy.init_node("slam_processor")
